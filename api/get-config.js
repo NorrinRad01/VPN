89f7fd83544a3f00349d466fc9283dc81522ccdb
NorrinRad01 <108555857+NorrinRad01@users.noreply.github.com>
2025-11-10 13:21:23 +0600

    Update get-config.js

--- api/get-config.js
+++ api/get-config.js
@@ -16,7 +16,7 @@ PersistentKeepalive = 25
 }
 
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://vpn-private.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
