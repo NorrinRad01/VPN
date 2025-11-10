a12a4912bf47d87300aa9854663ded23f2479e2f
NorrinRad01 <108555857+NorrinRad01@users.noreply.github.com>
2025-11-10 13:14:40 +0600

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,6 +1,6 @@
 // /api/create-payment (Vercel serverless)
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://vpn-private.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     
