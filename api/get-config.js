cfd006caca36eac937bffd7399dac9e4397ca333
NorrinRad01 <108555857+NorrinRad01@users.noreply.github.com>
2025-11-10 13:49:54 +0600

    Update get-config.js

--- api/get-config.js
+++ api/get-config.js
@@ -1,5 +1,7 @@
 // /api/get-config -- выдает WireGuard конфиг
 function generateWireGuardConfig(orderId) {
+    // В проде: тут вы бы обращались к базе
+    // Пока -- тестовый конфиг-шаблон с заглушкой
     return `[Interface]
 PrivateKey = <YOUR_CLIENT_PRIVATE_KEY>
 Address = 10.66.66.2/32
@@ -16,6 +18,7 @@ PersistentKeepalive = 25
 }
 
 export default async function handler(req, res) {
+    // CORS headers
     res.setHeader('Access-Control-Allow-Origin', 'https://vpn-private.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -29,6 +32,8 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: 'orderId required' });
     }
 
+    // В проде: проверить в БД, что заказ оплачен
+    // Макет: отдаем конфиг всегда.
     const configText = generateWireGuardConfig(orderId);
     return res.status(200).json({ configText });
 }
